e7d21d58d194d654e8901c804eb686199527019a
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-03-21 16:53:22 +0700

    FIX: Whitespace

--- code/GistShortCodeHandler.php
+++ code/GistShortCodeHandler.php
@@ -2,30 +2,23 @@
 
 class GistShortCodeHandler {
 
-    public static function parse_gist( $arguments, $caption = null, $parser = null ) {
-//  public static function link_shortcode_handler($arguments, $content = null, $parser = null) {
+	public static function parse_gist( $arguments, $caption = null, $parser = null ) {
+		// first things first, if we dont have a video ID, then we don't need to
+		// go any further
+		if ( empty( $arguments['id'] ) ) {
+			return;
+		}
 
-        error_log("GITHUB GIST CODE PARSER");
-        // first things first, if we dont have a video ID, then we don't need to
-        // go any further
-        if ( empty( $arguments['id'] ) ) {
-            return;
-        }
+		$customise = array();
+		$customise['GistID'] = $arguments['id'];
 
-        $customise = array();
-        /*** SET DEFAULTS ***/
-        $customise['GistID'] = $arguments['id'];
-       
+		//overide the defaults with the arguments supplied
+		$customise = array_merge( $customise, $arguments );
 
-        //overide the defaults with the arguments supplied
-        $customise = array_merge( $customise, $arguments );
+		//get our GIST template
+		$template = new SSViewer( 'GithubGist' );
 
-        //get our YouTube template
-        $template = new SSViewer( 'GithubGist' );
-
-        //return the customised template
-        return $template->process( new ArrayData( $customise ) );
-
-    }
+		//return the customised template
+		return $template->process( new ArrayData( $customise ) );
+	}
 }
-?>
