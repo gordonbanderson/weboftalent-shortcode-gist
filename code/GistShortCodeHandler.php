8d7e955e116990c17ba0db8dbc2c9fb195f67427
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-18 16:48:30 +0700

    FIX: Convert to PSR2.  Closes #4

--- code/GistShortCodeHandler.php
+++ code/GistShortCodeHandler.php
@@ -1,24 +1,25 @@
 <?php
 
-class GistShortCodeHandler {
+class GistShortCodeHandler
+{
+    public static function parse_gist($arguments, $caption = null, $parser = null)
+    {
+        // first things first, if we dont have a video ID, then we don't need to
+        // go any further
+        if (empty($arguments['id'])) {
+            return;
+        }
 
-	public static function parse_gist( $arguments, $caption = null, $parser = null ) {
-		// first things first, if we dont have a video ID, then we don't need to
-		// go any further
-		if ( empty( $arguments['id'] ) ) {
-			return;
-		}
+        $customise = array();
+        $customise['GistID'] = $arguments['id'];
 
-		$customise = array();
-		$customise['GistID'] = $arguments['id'];
+        //overide the defaults with the arguments supplied
+        $customise = array_merge($customise, $arguments);
 
-		//overide the defaults with the arguments supplied
-		$customise = array_merge( $customise, $arguments );
+        //get our GIST template
+        $template = new SSViewer('GithubGist');
 
-		//get our GIST template
-		$template = new SSViewer( 'GithubGist' );
-
-		//return the customised template
-		return $template->process( new ArrayData( $customise ) );
-	}
+        //return the customised template
+        return $template->process(new ArrayData($customise));
+    }
 }
