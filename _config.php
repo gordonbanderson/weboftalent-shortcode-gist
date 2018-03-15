31e0f1b80879b0194bedb303abfae71a213f29ff
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-03-15 20:50:52 +0700

    FIX: Remove echos

--- _config.php
+++ _config.php
@@ -1,6 +1,3 @@
 <?php
 use SilverStripe\View\Parsers\ShortcodeParser;
-echo '**** FROM GIST MODULE ****';
 ShortcodeParser::get('default')->register('Gist',array('WebOfTalent\ShortCodeGist\GistShortCodeHandler','handle_shortcode'));
-echo '**** /FROM GIST MODULE ****';
-
